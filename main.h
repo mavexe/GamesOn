89d784e042ef03efb3fe4d3e65133ad8f30e2908
Damir Mustafin <82767270+mavexe@users.noreply.github.com>
2024-12-04 10:41:50 +0500

    Add files via upload

--- main.h
+++ main.h
@@ -1,31 +1,5 @@
 
 #include <iostream>
+#include <stdio.h>
 #include <vector>
-#include "stdio.h"
-#include<iostream>
-#include "string"
-#include <vector>
-#include <ncurses.h>
-#include <stdlib.h>
-#include <ctime>
-
-const int WIDTH = 20;
-const int WEIGHT =20;
-
-const int DROP_INTERVAL = 500000*2;
-#pragma once
-
-#ifndef WRAPPED_NCURSES.H
-#define WRAPPED_NCURSES_H
-#include <ncurses.h>
-namespace ncurses{
-    using::move;
-}
-#endif
-
-bool canMove(int dx, int dy);
-void draw();
-void rotate();
-void clearLines();
-bool isBlockAbottomorAnotherBlock();
-
+#include <string>
